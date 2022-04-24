65ddd1752e9aa532637f9454a71f445cfbe4515e
Harri <harri_1992@163.com>
2022-04-24 09:19:52 +0800

    prettier

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const {app, BrowserWindow} = require('electron');
+const {
+    app,
+    BrowserWindow
+} = require('electron');
 const path = require("path");
 
 const createWindow = () => {
@@ -11,14 +14,14 @@ const createWindow = () => {
 
 app.on("ready", createWindow);
 
-app.on("window-all-closed", ()=>{
-    if(process.platform !== "darwin") {
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 })
 
-app.on("activate", ()=>{
-    if(BrowserWindow.getAllWindows.length === 0) {
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows.length === 0) {
         createWindow();
     }
 })
